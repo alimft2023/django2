c3d23a13a3a47327bc7d193dc1842389296e5d46
aliasgharsalehi <aliideno2010@gmail.com>
2023-10-03 21:05:27 +0330

    'final3'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
