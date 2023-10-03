11061ef12b2a33865de12a07783a61377a4d0c1b
aliasgharsalehi <aliideno2010@gmail.com>
2023-10-03 14:17:34 +0330

    'init'

---
+++ accounts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def login(request):
+    return render(request, "accounts/login.html", {})
+
+
+def register(request):
+    return render(request, "accounts/register.html", {})
+
+
+def addeducation(request):
+    return render(request, "accounts/add-education.html", {})
+
+
+def addexperience(request):
+    return render(request, "accounts/add-experience.html", {})
+
+
+def createprofile(request):
+    return render(request, "accounts/createprofile.html", {})
+
+
+def dashboard(request):
+    return render(request, "accounts/dashboard.html", {})
+
+
+def profile(request):
+    return render(request, "accounts/profile.html", {})
+
+
+def profiles(request):
+    return render(request, "accounts/profiles.html", {})
