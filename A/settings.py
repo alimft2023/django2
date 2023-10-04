b6ba97caba5553523edf3662168acef3391b6e39
aliasgharsalehi <aliideno2010@gmail.com>
2023-10-04 20:05:01 +0330

    'final07'

--- A/settings.py
+++ A/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "blog" / "static",
 ]
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
