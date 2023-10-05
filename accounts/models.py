5996c5566f3e285502db471e4518a4b2a545eb3e
aliasgharsalehi <aliideno2010@gmail.com>
2023-10-05 05:41:53 +0330

    'final15'

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,15 @@ class UserProfile(models.Model):
     bio = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(upload_to="profile_picture")
+
+
+class Education(models.Model):
+    school = models.CharField(max_length=100)
+    degree = models.CharField(max_length=100)
+    field = models.models.CharField(max_length=100)
+    start = models.DateTimeField()
+    end = models.DateTimeField(null=True)
+    description = models.text()
+
+class Experience(models.Model):
+    
